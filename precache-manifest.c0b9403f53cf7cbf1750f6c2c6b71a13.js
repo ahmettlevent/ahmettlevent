dc43e45dfd97d991c25e917e61a86f57513aeb7c
Ahmet Utku Levent <ahmettlevent@gmail.com>
2020-08-02 23:20:46 +0300

    pushed react website

---
+++ precache-manifest.c0b9403f53cf7cbf1750f6c2c6b71a13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caa541d58755aa18de0c36126991dc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ae1d09e1aabfa5d1765",
+    "url": "/static/css/main.d7154ffd.chunk.css"
+  },
+  {
+    "revision": "6b6c4535b0e0f653a87e",
+    "url": "/static/js/2.12abef7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.12abef7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ae1d09e1aabfa5d1765",
+    "url": "/static/js/main.b22519ec.chunk.js"
+  },
+  {
+    "revision": "71ec19f52023772edbeb",
+    "url": "/static/js/runtime-main.5f8b155f.js"
+  },
+  {
+    "revision": "c53b810dfd3d244c0fd4192749ef141e",
+    "url": "/static/media/pp.c53b810d.jpg"
+  }
+]);
